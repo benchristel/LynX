2ad29a665894115e002c959176a6aa77993e2331
Ben Christel <ben.christel@gmail.com>
2022-05-21 17:29:42 -0700

    Document Firefox alt-key fix

--- setup.sh
+++ setup.sh
@@ -54,4 +54,6 @@ EOF
 run sudo su root -c "echo '$USER ALL=NOPASSWD:/home/$USER/workspace/xkeysnail-macos-keymap/run' >/etc/sudoers.d/xkeysnail"
 run sudo su root -c "chmod 440 /etc/sudoers.d/xkeysnail"
 
+manual "In the Firefox location bar, go to about:config, search for ui.key.menuAccessKeyFocuses, and set the Value to false."
+
 manual "Reboot for changes to take effect."
