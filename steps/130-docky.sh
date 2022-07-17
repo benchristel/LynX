8fe591b47ac4005ea9b0974039dd1a501acf4065
Ben Christel <ben.christel@gmail.com>
2022-07-17 14:05:22 -0700

    Configure Docky

--- steps/130-docky.sh
+++ steps/130-docky.sh
@@ -23,7 +23,38 @@ fi
 # configure docky
 gconftool-2 --set "/apps/docky-2/Docky/Services/ThemeService/UrgentHue" --type int 0
 gconftool-2 --set "/apps/docky-2/Docky/Items/DockyItem/ShowDockyItem" --type bool false
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/Launchers" \
+  --type list --list-type string \
+"[file:///usr/share/applications/nemo.desktop,\
+file:///usr/share/applications/mintinstall-kde.desktop,\
+file:///usr/share/applications/firefox.desktop,\
+file:///usr/share/applications/rhythmbox.desktop,\
+file:///usr/share/applications/libreoffice-writer.desktop,\
+file:///usr/share/applications/libreoffice-calc.desktop,\
+file:///usr/share/applications/libreoffice-impress.desktop,\
+file:///usr/share/applications/thunderbird.desktop,\
+file:///usr/share/applications/org.gnome.Calendar.desktop,\
+file:///usr/share/applications/cinnamon-settings.desktop]"
+
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/SortList" \
+  --type list --list-type string \
+"[/usr/share/applications/nemo.desktop,\
+/usr/share/applications/mintinstall-kde.desktop,\
+/usr/share/applications/firefox.desktop,\
+/usr/share/applications/rhythmbox.desktop,\
+/usr/share/applications/libreoffice-writer.desktop,\
+/usr/share/applications/libreoffice-calc.desktop,\
+/usr/share/applications/libreoffice-impress.desktop,\
+/usr/share/applications/org.gnome.Calendar.desktop,\
+/usr/share/applications/cinnamon-settings.desktop,\
+TrashCan]"
 
 # install Frost theme
 lib/github-clone.sh benchristel/docky-frost ~/.local/share/docky/themes/Frost
 gconftool-2 --set "/apps/docky-2/Docky/Services/ThemeService/Theme" --type string Frost
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/IconSize" --type int 64
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/IndicateMultipleWindows" --type bool false
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/ThreeDimensional" --type bool true
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/ZoomEnabled" --type bool true
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/ZoomPercent" --type float 1.5
+gconftool-2 --set "/apps/docky-2/Docky/Interface/DockPreferences/Dock1/Plugins" --type list --list-type string "[Trash]"
