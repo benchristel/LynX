acc9849f55ccdae25f766096abda14207ae9ce49
Ben Christel <ben.christel@gmail.com>
2022-07-17 17:59:32 -0700

    Fix .cinnamon/configs race condition

--- steps/lib/github-clone.sh
+++ steps/lib/github-clone.sh
@@ -6,5 +6,11 @@ DEST="${2:-"$HOME/workspace/$REPO"}"
 mkdir -p "$(dirname "$DEST")"
 if ! [ -d "$DEST" ]; then
     echo git clone "https://github.com/${REPO}.git" "$DEST"
-    git clone "https://github.com/${REPO}.git" "$DEST"
+
+    # clone and checkout as separate steps to overwrite any existing files
+    # in the directory. This is needed for the ~/.cinnamon/configs directory,
+    # to work around a race condition where Cinnamon writes to a file in the
+    # directory while the clone is in progress.
+    git clone --no-checkout "https://github.com/${REPO}.git" "$DEST"
+    git -C "$DEST" checkout -f HEAD
 fi
