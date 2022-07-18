9e9af82ae8111d952b3ee94809b486c7fd02d2b6
Ben Christel <ben.christel@gmail.com>
2022-07-17 21:08:16 -0700

    When deciding whether to clone a repo, check for .git, not the repo dir
    This ensures that if Cinnamon recreates ~/.cinnamon/configs right after
    we delete it, we'll still clone the repo.

--- steps/lib/github-clone.sh
+++ steps/lib/github-clone.sh
@@ -4,13 +4,16 @@ REPO="$1"
 DEST="${2:-"$HOME/workspace/$REPO"}"
 
 mkdir -p "$(dirname "$DEST")"
-if ! [ -d "$DEST" ]; then
-    echo git clone "https://github.com/${REPO}.git" "$DEST"
-
-    # clone and checkout as separate steps to overwrite any existing files
-    # in the directory. This is needed for the ~/.cinnamon/configs directory,
-    # to work around a race condition where Cinnamon writes to a file in the
-    # directory while the clone is in progress.
-    git clone --no-checkout "https://github.com/${REPO}.git" "$DEST"
-    git -C "$DEST" checkout -f HEAD
+if [ -d "$DEST/.git" ]; then
+    echo "$DEST already exists and is a Git repo. Skipping."
+    exit 0
 fi
+
+echo git clone "https://github.com/${REPO}.git" "$DEST"
+
+# clone and checkout as separate steps to overwrite any existing files
+# in the directory. This is needed for the ~/.cinnamon/configs directory,
+# to work around a race condition where Cinnamon writes to a file in the
+# directory while the clone is in progress.
+git clone --no-checkout "https://github.com/${REPO}.git" "$DEST"
+git -C "$DEST" checkout -f HEAD
