7cc972d863901edd3c9e5cfcde6c355418fdaf12
Ben Christel <ben.christel@gmail.com>
2022-07-18 19:57:06 -0700

    Avoid reinstalling chruby for every new user

--- steps/60-ruby.sh
+++ steps/60-ruby.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eu
 
 # install chruby
-if ! [ -f ~/.bashrc.d/chruby ]; then
-    pushd /tmp
+if ! [ -f /usr/local/share/chruby/chruby.sh ]; then
+    pushd "$TMP"
         rm -rf chruby-0.3.9
         wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
         tar -xzvf chruby-0.3.9.tar.gz
@@ -14,7 +14,7 @@ fi
 
 # install ruby-install
 if ! >/dev/null which ruby-install; then
-    pushd /tmp
+    pushd "$TMP"
         rm -rf ruby-install-0.8.3
         wget -O ruby-install-0.8.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.8.3.tar.gz
         tar -xzvf ruby-install-0.8.3.tar.gz
